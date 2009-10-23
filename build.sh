ca2a4c373e3ca279be9a48945209c302fffd403a
David Wolinsky <davidiw@ufl.edu>
2009-10-22 21:40:48 -0400

    Proper creation for interaction with apt-repositories

--- build.sh
+++ build.sh
@@ -10,12 +10,13 @@ version=$2
 base_dir=condor_deb
 package_dir=$base_dir/opt/condor
 mkdir -p $package_dir
-mv $path $package_dir/.
+cp -axf $path/* $package_dir/.
 
 debian_dir=$base_dir/DEBIAN
 mkdir -p $debian_dir
 cp condor_control $debian_dir/control
 echo "Version: $version" >> $debian_dir/control
 
-dpkg-deb -b $package_dir
-rm -rf $package_dir
+dpkg-deb -b $base_dir
+rm -rf $base_dir
+mv $base_dir.deb condor_$version.deb
